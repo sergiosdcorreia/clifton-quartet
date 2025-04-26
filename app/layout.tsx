be45adeaf8fcb27b9febf18b549db1fd609f7c51
Sergio Correia <sergiosdcorreia@gmail.com>
2025-04-26 01:30:17 +0100

    Adding footer with copyrights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Instrument_Serif } from "next/font/google";
 import "./globals.css";
 import { ReactLenis } from "@/utils/lenis";
+import Footer from "@/components/Footer";
 
 const instrumentSerif = Instrument_Serif({
   weight: "400",
@@ -19,6 +20,7 @@ export default function RootLayout({
         <body className={`${instrumentSerif.variable} antialiased`}>
           {children}
         </body>
+        <Footer />
       </ReactLenis>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col items-center m-4">
+      <p className="text-center text-white text-sm">
+        © 2025 The Clifton String Quartet
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
